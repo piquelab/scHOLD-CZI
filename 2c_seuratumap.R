ce211b39e20c35df63d27bc6f980fe32dffedf17
Cindy <cakalita@gmail.com>
2024-03-06 13:47:22 -0500

    split scripts so that umap is run once so all resolution check maps are the same

---
+++ 2c_seuratumap.R
@@ -0,0 +1,33 @@
+library(Seurat)
+library(future)
+library(tidyverse)
+library(harmony)
+
+args <- commandArgs(trailingOnly = TRUE)
+#args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/",10) #for testing
+base <- args[1]
+outFolder=paste0(base,"2.1_mergeCellRangerAndDemuxlet_renamed/")
+if (!file.exists(outFolder)) dir.create(outFolder, showWarnings=F)
+
+basefolder=gsub("analysis/","counts_cellranger_hg38/",base)
+
+# set new output dir for filtered out unmatched figures
+figuredir=paste0(outFolder,"figures/")
+if (!file.exists(figuredir)) dir.create(figuredir, showWarnings=F)
+
+dimset <- as.numeric(args[2])
+
+future::plan(strategy = 'multicore', workers = 10)
+options(future.globals.maxSize = 30 * 1024 ^ 3)
+
+########################
+
+opfn_i <- file.info(dir(outFolder, full.names=T, pattern="^seuratObj-afterharmony."))
+opfn <- rownames(opfn_i)[which.max(opfn_i$mtime)]
+sc <- read_rds(opfn)
+
+sc <- sc %>% RunUMAP(reduction = "harmony", dims = 1:dimset) 
+sc <- sc %>% FindNeighbors(reduction = "harmony", dims = 1:dimset)
+
+opfn <- paste0(outFolder,"seuratObj-post-umap",".",Sys.Date(),".rds")
+write_rds(sc, opfn)
