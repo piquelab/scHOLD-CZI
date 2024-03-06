4aa5acb3214a872c19e0f6be60a87e34e56f1f99
Cindy <cakalita@gmail.com>
2024-03-06 13:51:01 -0500

    fixed to qc missing samples here. added table columns that were added in later scripts. subsetting table to only use batch and individual combinations that exist in covariate table

--- 1_demux.R
+++ 1_demux.R
@@ -6,15 +6,18 @@ library(tidyverse)
 library(parallel)
 library(data.table)
 
-rm(list=ls())
-
 args <- commandArgs(trailingOnly = TRUE)
-#args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/","/rs/rs_grp_schold/CZI/RNA/counts_cellranger_hg38/demuxlet/demuxlet/","CZ1_group.txt") #for testing
+#args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/","/rs/rs_grp_schold/CZI/RNA/counts_cellranger_hg38/demuxlet/demuxlet/","/rs/rs_grp_schold/covariates/HOLD-CZI_covariates_HOLD01-HOLD14_dbgap.ID_cziexp_03_04_2024.txt","CZ1_group.txt") #for testing
 analysis=args[1]
 basefolder=args[2]
-if(!is.na(args[3])){
-samples=fread(args[3],header=F)$V1
+if(!is.na(args[4])){
+samples=read.table(args[4],header=F)
+samples$Batch <- sapply(strsplit(samples$V1,"-"),function(y) y[1])
 }
+cov_file=args[3]
+exp <- read.table(cov_file, row.names=NULL,header=T)
+exp$Batch <- gsub("HOLD0","HOLD",exp$Batch)
+exp <- transform(exp,ID_Batch= paste0(dbgap.ID, "_", Batch))
 outdir <- paste0(analysis,"1_demux_output/")
 if (!file.exists(outdir)) dir.create(outdir, showWarnings=F)
 
@@ -25,8 +28,9 @@ demuxfn <- list.files(basefolder,pattern="*.out.best")
 
 expNames_in <- gsub(".out.best", "", demuxfn) 
 
-if(!is.na(args[3])){
-expNames <- expNames_in[expNames_in %in% samples] #run subset of all output in the folder
+if(!is.na(args[4])){
+expNames <- expNames_in[expNames_in %in% samples$V1] #run subset of all output in the folder
+exp <- exp %>% dplyr::filter(Batch %in% samples$Batch)
 } else {
 expNames <- expNames_in
 }
@@ -37,22 +41,27 @@ expNames <- expNames_in
 
 # new column "NEW_BARCODE" was added to the data frame associated to each experiment (experiment name+barcode) "-1" was removed from end of barcode
 demux <- mclapply(expNames,function(ii){
+  #ii <- expNames[1]
   cat("#Loading ", ii, "\n")
   fn <- paste0(basefolder, ii,".out.best")
   dd <- data.frame(fread(fn,header=T))
-  dd <- dd%>%mutate(NEW_BARCODE=paste0(ii,"_", gsub("-1","",BARCODE)),EXP=ii)
+  dd <- dd%>%mutate(NEW_BARCODE=paste0(ii,"_", gsub("-1","",BARCODE)),EXP=ii,Sample_ID=SNG.BEST.GUESS)
+  missing <- setdiff(exp$dbgap.ID, dd$Sample_ID)
+  IDmiss <- exp %>% dplyr::filter(dbgap.ID %in% missing) %>% mutate(Sample_ID=dbgap.ID) %>% select(dbgap.ID, Batch, Sample_ID) 
+  write.csv(IDmiss, paste0(outdir,"missing_samples_in_demultiplexing_results.csv"), row.names=F)
+  dd <- transform(dd, BATCH=gsub("-.*", "", EXP), treats=gsub(".*[0-9].{,2}-","",EXP))
+  dd <- dd[dd$Sample_ID %in% exp$dbgap.ID,] #run subset of all output in the folder
+  return(dd)
 },mc.cores=10)
 
 # merging all the experiments 
 demux <- do.call(rbind,demux)
 
 ### output
-opfn <- paste0(outdir,"1_demux_New.ALL.rds");
+opfn <- paste0(outdir,"1_demux_New.ALL.rds")
 write_rds(demux, opfn)
 
 # DROPLET.TYPE: "SNG" "AMB"
-opfn <- paste0(outdir,"1_demux_New.SNG.rds");
-demux %>% filter(DROPLET.TYPE=="SNG") %>%
-    write_rds(opfn)
-
-
+opfn <- paste0(outdir,"1_demux_New.SNG.rds")
+demux <- subset(demux,DROPLET.TYPE=="SNG")
+write_rds(demux,opfn)
