970258c801636eb69b3ec9622d7fc8474b16b69b
Cindy <cakalita@gmail.com>
2024-09-04 12:37:03 -0400

    no kin in cov file. fixed plotting. added genotype PC by discrimination plot.

--- DESeq_plotting.R
+++ DESeq_plotting.R
@@ -8,47 +8,70 @@ library(DESeq2)
 
 
 args <- commandArgs(trailingOnly = TRUE)
-#args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/") #for testing
+args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/","/rs/rs_grp_schold/covariates/HOLD-CZI_covariates_converted_dbgapID_key_n183_AR.txt","ALL","fastdemux") #for testing
 base <- args[1]
-outFolder=paste0(base,"pseudobulk_ctrl/")
+cov_file=fread(args[2]) #this is the psych cov file
+project=args[3]
+method=args[4]
+outFolder=paste0(base,method,"_pseudobulk_ctrl/")
 figuredir=paste0(outFolder,"figures/")
 
 filenames <- list.files(outFolder) #file list from directory
 filenames1 <- filenames[grep("DESeq_output-", filenames)] #pick specific files from list
 x <- na.omit(sapply(strsplit(filenames1,"(?<=.)(?=C[0-9])",perl=TRUE),function(y)y[2]))
 clusters <- unique(sapply(strsplit(x,"[.]"),function(z)z[1]))
-eigenvec2_o <- fread(file=paste0(base,"genotypePC/eigenvec_pc.txt")) #will use col PC1
+eigenvec2_o <- fread(file=paste0(base,"genotypePCnokin/",project,".eigenvec_pc.txt")) #will use col PC1
 eigenvec2 <- merge(eigenvec2_o,cov_file,by.x="Sample_ID",by.y="dbgap.ID",all.x=T)
 notrun_var <- c("DSES_01","DSES_03","PWaist","PHip")
 colnumuotovar <- grep("czi_exp",colnames(eigenvec2))+1
 psychvarstorun <- eigenvec2[,colnumuotovar:length(colnames(eigenvec2))]
 psychvarstorun <- colnames(psychvarstorun)[!colnames(psychvarstorun) %in% notrun_var]
 
+cluster=clusters[1]
+ var="LPSvsCTRL"
+opfn <- paste0(outFolder,project,".DESeq_countlists.RData")
+load(opfn)
+
+load(paste0(outFolder,project,".DESeq_output-",var,cluster,".RData"))
+annotation <- metadata_ls[[cluster]][match(rownames(colData(dds)), rownames(metadata_ls[[cluster]])),]
+
+#mcols(dds) <- cbind(mcols(dds), metadata_ls[[cluster]])
+colData(dds) <- cbind(colData(dds), annotation)
+
+#reorder_idx = match(rownames(colData(dds)),rownames(metadata_ls[[cluster]])  )
+#reordered <- metadata_ls[[cluster]][reorder_idx,]  
+
+    vsd <- vst(dds, blind=FALSE)
+
+
+
+    # Plot PCA
+    fig0 <- DESeq2::plotPCA(vsd, intgroup = c("treats"))+ggtitle("treats")+theme(plot.margin=margin(b=-0.8,unit="cm"))
+    fig1 <- DESeq2::plotPCA(vsd, intgroup = c("sex_alph"))+ggtitle("sex")+theme(plot.margin=margin(b=-0.8,unit="cm"))
+    fig2 <- DESeq2::plotPCA(vsd, intgroup = c("age"))+ggtitle("age")+theme(plot.margin=margin(b=-0.8,unit="cm"))
+    fig3 <- DESeq2::plotPCA(vsd, intgroup = c("BATCH"))+ggtitle("BATCH")+theme(plot.margin=margin(b=-0.8,unit="cm"))
+    fig <-plot_grid(fig0, fig1,fig2,fig3, nrow=4, ncol=1, align="h")
+    png(paste0(figuredir,project,".deseqPCA-treats",var,"-",cluster,".png"), width=1000, height=1000, res=120)
+    print(fig)
+    dev.off()
+
+pca <-DESeq2::plotPCA(vsd, intgroup = c("treats"),returnData=TRUE)
+outlier <- pca[pca$treats=="RNA-CTRL" & pca$PC1<(-20) & pca$PC2<0,]
+
+
 for (cluster in clusters){
     #cluster=clusters[1]
     cat("running ", cluster, "\n")
     var="LPSvsCTRL"
-    load(paste0(outFolder,"DESeq_output-",var,cluster,".RData"))
 
-    vsd <- vst(dds, blind=FALSE)
-    # Plot PCA
-    fig0 <- DESeq2::plotPCA(vsd, intgroup = c("treats"))+ggtitle("treats")+theme(plot.margin=margin(b=-0.8,unit="cm"))
-    fig1 <- DESeq2::plotPCA(vsd, intgroup = c("BATCH"))+ggtitle("BATCH")+theme(plot.margin=margin(b=-0.8,unit="cm"))
-    fig2 <- DESeq2::plotPCA(vsd, intgroup = c("sex_alph"))+ggtitle("sex")+theme(plot.margin=margin(b=-0.8,unit="cm"))
-    fig3 <- DESeq2::plotPCA(vsd, intgroup = c("age"))+ggtitle("age")+theme(plot.margin=margin(b=-0.8,unit="cm"))
-    fig <-plot_grid(fig0, fig1, fig2,fig3, nrow=4, ncol=1, align="h")
-    png(paste0(figuredir,"deseqPCA-",var,"-",cluster,".png"), width=1000, height=1000, res=120)
-    print(fig)
-    dev.off()
-
-    sub.table <- fread(paste0(outFolder,"deseqres_",var,".txt"))
+    sub.table <- fread(paste0(outFolder,project,".deseqres_",var,".txt"))
     colnames(sub.table) <- c('identifier', 'padj', 'pvalue', 'logFC','var','Cluster')
     sub.table <- subset(sub.table, Cluster==cluster)
     sub.table <- sub.table[order(sub.table$padj,-abs(sub.table$logFC)), ]
 
     topp1 <- min(subset(sub.table, padj > quantile(padj, prob = 1 -99/100,na.rm=T))$pvalue)
     tolab <- c(unique(head(sub.table,n=20)$identifier))
-    png(width = 8, height = 8, file=paste0(figuredir,"dge_volcano-",var,"-",cluster,".png"), pointsize=12, 
+    png(width = 8, height = 8, file=paste0(figuredir,project,".dge_volcano-",var,"-",cluster,".png"), pointsize=12, 
           bg = "transparent", units = "in", res = 1200)
     p <- EnhancedVolcano(sub.table,
       lab = sub.table$identifier,
@@ -79,14 +102,14 @@ for (cluster in clusters){
         #var="sex"
         #load(paste0(outFolder,"DESeq_output-",i,"-",var,cluster,".RData"))
         cat("running ",var,i)
-        sub.table <- fread(paste0(outFolder,"deseqres_",var,"-",i,".txt"))
+        sub.table <- fread(paste0(outFolder,project,".deseqres_",var,"-",i,".txt"))
             colnames(sub.table) <- c('identifier', 'padj', 'pvalue', 'logFC','var','Cluster','treats')
         sub.table <- subset(sub.table, Cluster==cluster & treats==i)
         sub.table <- sub.table[order(sub.table$padj,-abs(sub.table$logFC)), ]
 
         topp1 <- min(subset(sub.table, padj > quantile(padj, prob = 1 -99/100,na.rm=T))$pvalue)
         tolab <- c(unique(head(sub.table,n=20)$identifier))
-        png(width = 8, height = 8, file=paste0(figuredir,"dge_volcano-",var,"-",cluster,"-",i,".png"), pointsize=12, 
+        png(width = 8, height = 8, file=paste0(figuredir,project,".dge_volcano-",var,"-",cluster,"-",i,".png"), pointsize=12, 
               bg = "transparent", units = "in", res = 1200)
         p <- EnhancedVolcano(sub.table,
           lab = sub.table$identifier,
@@ -212,6 +235,48 @@ ggcorrplot(myMat, method = "square", outline.col = "grey", ggtheme = ggplot2::th
 scale_fill_gradient2(low = "blue", high =  "red", mid = "white", midpoint = 0)
     #theme(axis.text.x = element_text(angle = 45, hjust = 1, colour = a))
 dev.off()
+
+
+#plot genotype PC by discrimination -- does genotype removal from the model remove the discrimination
+library(ggpubr)
+
+p <- ggplot(eigenvec2, aes(x=PC1, y=EDS_mean)) +
+  theme_bw()+
+  geom_point()+ #aes(color=sig)
+  geom_vline(xintercept = 0)+
+  geom_hline(yintercept = 0)+
+  geom_smooth(method = "lm", fill = NA)+
+  ylab("Discrimination") +
+  xlab("Genotype PC1") +
+#  geom_text(aes(x=-Inf, y=Inf, hjust=-0.2, vjust=1.2, label = r2_eqn(lm(as.numeric(AF) ~ as.numeric(value)))), data=bbinom_ind_pop[bbinom_ind_pop$cell %in% c(unique(snp1$cell))], parse = TRUE, size=6,colour="black") 
+  stat_cor(color="blue",method="spearman",cor.coef.name = "rho", size=6, label.sep="\n", r.digits=2,na.rm=T)+ #label.x = -6,label.y = 5
+    theme(panel.background = element_rect(fill="white",colour = "black",size=1.3),
+    axis.text.x = element_text(colour = "black",size = rel(1.3)),axis.text.y = element_text(colour = "black",size = rel(1.3)),
+    axis.title.y = element_text(colour = "black",size = rel(1.5)),axis.title.x = element_text(colour = "black",size = rel(1.5)),
+    legend.text=element_text(size = rel(1.3)),legend.title=element_text(size = rel(1.5)),strip.text.x = element_text(size = rel(1.3))) #+ coord_cartesian(ylim = c(-8,8), xlim = c(-8,8))
+
+png(width = 12, height = 12, file=paste0(figuredir,project,".PC1_vs_EDS.png"), pointsize=12, 
+      bg = "transparent", canvas = "white", units = "in", res = 1200)
+print(p)
+dev.off()
+
+#CRP values , highlight HO-003, HO-123, HO-183
+eigenvec2$all <- "All"
+p <- ggplot(eigenvec2, aes(x=all,y=HS_CRP)) +
+  theme_bw()+
+  geom_boxplot()+ #aes(color=sig)
+  geom_point(data=subset(eigenvec2, Sample_ID %in% c("HO-003", "HO-123", "HO-183")),aes(color="red"))+
+#  geom_text(aes(x=-Inf, y=Inf, hjust=-0.2, vjust=1.2, label = r2_eqn(lm(as.numeric(AF) ~ as.numeric(value)))), data=bbinom_ind_pop[bbinom_ind_pop$cell %in% c(unique(snp1$cell))], parse = TRUE, size=6,colour="black") 
+    theme(panel.background = element_rect(fill="white",colour = "black",size=1.3),
+    axis.text.x = element_text(colour = "black",size = rel(1.3)),axis.text.y = element_text(colour = "black",size = rel(1.3)),
+    axis.title.y = element_text(colour = "black",size = rel(1.5)),axis.title.x = element_text(colour = "black",size = rel(1.5)),
+    legend.text=element_text(size = rel(1.3)),legend.title=element_text(size = rel(1.5)),strip.text.x = element_text(size = rel(1.3))) #+ coord_cartesian(ylim = c(-8,8), xlim = c(-8,8))
+
+png(width = 12, height = 12, file=paste0(figuredir,project,".CRP_box.png"), pointsize=12, 
+      bg = "transparent", canvas = "white", units = "in", res = 1200)
+print(p)
+dev.off()
+
 
 
 
