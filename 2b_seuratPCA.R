e30c1a7064ef6ac3e8718e5d17e2fd908026885d
Cindy <cakalita@gmail.com>
2025-08-27 13:36:50 -0400

    updated for no dex

--- 2b_seuratPCA.R
+++ 2b_seuratPCA.R
@@ -4,11 +4,12 @@ library(tidyverse)
 library(harmony)
 
 args <- commandArgs(trailingOnly = TRUE)
-#args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/","ALL","fastdemux") #for testing
-#args <- c("/rs/rs_grp_scaloft/scALOFT_2024/cindy_analysis/","ALL","demux")
+#args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/","ALL","fastdemux","CZI") #for testing
+args <- c("/rs/rs_grp_scaloft/scALOFT_2024/cindy_analysis/","ALL","demux","ALOFT")
 base <- args[1]
 project <- args[2]
 method <- args[3]
+job <- args[4]
 outFolder=paste0(base,"2.1_mergeCellRangerAnd",method,"/")
 if (!file.exists(outFolder)) dir.create(outFolder, showWarnings=F)
 
@@ -16,8 +17,8 @@ if (!file.exists(outFolder)) dir.create(outFolder, showWarnings=F)
 figuredir=paste0(outFolder,"figures/")
 if (!file.exists(figuredir)) dir.create(figuredir, showWarnings=F)
 
-future::plan(strategy = 'multicore', workers = 2)
-options(future.globals.maxSize = 30 * 1024 ^ 3)
+future::plan(strategy = 'multicore', workers = 6)
+options(future.globals.maxSize = 100 * 1024 ^ 3)
 
 ##############################
 
@@ -27,25 +28,33 @@ opfn <- rownames(opfn_i)[which.max(opfn_i$mtime)]
 sc <- readRDS(opfn)
 
 #sc <- subset(sc, subset = nFeature_RNA > 200 & nFeature_RNA < 5000 & percent.mt < 5) # could try mt<10 to increase data
+if(job=="CZI"){
+sc <- subset(sc,subset=treats != "RNA-LPS-DEX") 
+} else if(job=="ALOFT"){
+	sc <- subset(sc,subset=treats != "LPS-DEX") 
+}
+filter="noDEX"
 
 sc <- NormalizeData(sc)
 sc <- FindVariableFeatures(sc, selection.method = "vst", nfeatures = 3000)
 sc <- ScaleData(sc, features = rownames(sc)) #can regress out highly variable input with flag: vars.to.regress = "percent.mt"
 sc <- RunPCA(sc, features = VariableFeatures(object = sc))
 #sc <- RunPCA(sc,pc.genes = sc@var.genes, npcs = 100, verbose = TRUE)
-opfn <- paste0(outFolder,project,".seuratObj-afterPCA.",Sys.Date(),".rds") 
+opfn <- paste0(outFolder,project,".seuratObj-afterPCA.",filter,".rds") 
 write_rds(sc, opfn)
 
-png(width=1000, height=1000, res=120, file=paste0(figuredir,project,".pca1-15_heatmap_QC.png"), bg = "transparent")
+png(width=1000, height=1000, res=120, file=paste0(figuredir,project,".",filter,".pca1-15_heatmap_QC.png"), bg = "transparent")
 p <- DimHeatmap(sc, dims = 1:15, cells = 500, balanced = TRUE) # for multiple PCs
 print(p)
 dev.off()
-png(width = 1000, height = 1000, file=paste0(figuredir,project,".elbowplot_QC.png"), bg = "transparent", res = 120)
+png(width = 1000, height = 1000, file=paste0(figuredir,project,".",filter,".elbowplot_QC.png"), bg = "transparent", res = 120)
 p <- ElbowPlot(sc)
 print(p)
 dev.off()
 
-sc <- RunHarmony(sc,"Library")#had to remove reduction="pca" with SeuratV5
+gc()
 
-opfn <- paste0(outFolder,project,".seuratObj-afterharmony.",Sys.Date(),".rds") 
+sc <- RunHarmony(sc,"Library")#had to remove reduction="pca" with SeuratV5 #kmeans_init_nstart=100, kmeans_init_iter_max=5000 did not remove quick transfe warning
+
+opfn <- paste0(outFolder,project,".seuratObj-afterharmony.",filter,".rds") 
 write_rds(sc, opfn)
--- 2c_seuratumap.R
+++ 2c_seuratumap.R
@@ -4,12 +4,13 @@ library(tidyverse)
 library(harmony)
 
 args <- commandArgs(trailingOnly = TRUE)
-#args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/",11,"ALL","fastdemux") #for testing
-#args <- c("/rs/rs_grp_scaloft/scALOFT_2024/cindy_analysis/",13,"ALL","demux")
+#args <- c("/rs/rs_grp_schold/CZI/RNA/analysis/",13,"ALL","fastdemux","CZI") #for testing
+args <- c("/rs/rs_grp_scaloft/scALOFT_2024/cindy_analysis/",13,"ALL","demux","ALOFT")
 
 base <- args[1]
 project <- args[3]
 method <- args[4]
+job <- args[5]
 outFolder=paste0(base,"2.1_mergeCellRangerAnd",method,"/")
 if (!file.exists(outFolder)) dir.create(outFolder, showWarnings=F)
 
@@ -20,22 +21,24 @@ figuredir=paste0(outFolder,"figures/")
 if (!file.exists(figuredir)) dir.create(figuredir, showWarnings=F)
 
 dim <- as.numeric(args[2])
+filter <- "noDEX"
 
-future::plan(strategy = 'multicore', workers = 4)
-options(future.globals.maxSize = 30 * 1024 ^ 3)
+future::plan(strategy = 'multicore', workers = 6)
+options(future.globals.maxSize = 100 * 1024 ^ 3)
 
 ########################
 
 #test both dim picked from elbowplot and default
 for (dimset in c(dim,50)){ 
 	cat("running",dimset,"\n")
-opfn_i <- file.info(dir(outFolder, full.names=T, pattern=paste0(project,".seuratObj-afterharmony.")))
-opfn <- rownames(opfn_i)[which.max(opfn_i$mtime)]
+#opfn_i <- file.info(dir(outFolder, full.names=T, pattern=paste0(project,".seuratObj-afterharmony.")))
+#opfn <- rownames(opfn_i)[which.max(opfn_i$mtime)]
+opfn <- paste0(outFolder,project,".seuratObj-afterharmony.",filter,".rds")
 sc <- read_rds(opfn)
 
 sc <- sc %>% RunUMAP(reduction = "harmony", dims = 1:dimset) 
 sc <- sc %>% FindNeighbors(reduction = "harmony", dims = 1:dimset)
 
-opfn <- paste0(outFolder,project,".seuratObj-post-umap.",dimset,".rds")
+opfn <- paste0(outFolder,project,".seuratObj-post-umap.",dimset,".",filter,".rds")
 write_rds(sc, opfn)
 }
